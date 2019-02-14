7257cd5c2767c3bf6e5076b84952dc62da40960b
yiannisefthyvoulou <yiannisefth@outlook.com>
2019-02-14 21:04:48 +0000

    Login and Registration screen functional
    Added a login and reg screen. Both fully functional and working as expected with the cloud database(Firestore)

---
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/LoginActivity.java
@@ -0,0 +1,186 @@
+package com2027.killaz.kalorie.gitfit;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Login activity, allows the user to sign in or request a password reset.
+ * Using shared preferences also allows to save login information for when the app is re-opened.
+ */
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button mLoginButton;
+    private Button mSignUpButton;
+    private TextView mForgotPassword;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private CheckBox mRememberCheckBox;
+    private SharedPreferences mPrefs = null;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+        mForgotPassword = (TextView) findViewById(R.id.forgotPassView);
+        mEditTextEmail = (EditText) findViewById(R.id.emailField);
+        mEditTextPassword = (EditText) findViewById(R.id.passwordField);
+        mRememberCheckBox = (CheckBox) findViewById(R.id.rememberCheckBox);
+        mProgressBar = (ProgressBar) findViewById(R.id.loginProgressBar);
+        mPrefs = getSharedPreferences(getString(R.string.shared_pref), MODE_PRIVATE);
+        if (mPrefs.getBoolean("rememberMe", true)) {
+            String rememberedEmail = mPrefs.getString("rememberedEmail", null);
+            String rememberedPassword = mPrefs.getString("rememberedPass", null);
+            mEditTextEmail.setText(rememberedEmail);
+            mEditTextPassword.setText(rememberedPassword);
+            mRememberCheckBox.setChecked(true);
+        }
+        userLogin();
+        forgotPasswordDialog();
+        signUpActivity();
+    }
+
+    /**
+     * Validates username and password and checks if the user exists in firebase and log them in.
+     * If any conditions are not met, such as empty or invalid fields or the account doesn't exist, the appropriate message is shown.
+     */
+    private void userLogin() {
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEditTextEmail.getText().toString().trim();
+                String password = mEditTextPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    mEditTextEmail.setError("Email is required");
+                    mEditTextEmail.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mEditTextEmail.setError("Please enter a valid email");
+                    mEditTextEmail.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    mEditTextPassword.setError("Password is required");
+                    mEditTextPassword.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mEditTextPassword.setError("Minimum length of password should be 6");
+                    mEditTextPassword.requestFocus();
+                    return;
+                }
+                if (mRememberCheckBox.isChecked()) {
+                    mPrefs.edit().putBoolean("rememberMe", true).apply();
+                    mPrefs.edit().putString("rememberedEmail", email).apply();
+                    mPrefs.edit().putString("rememberedPass", password).apply();
+                } else if (!mRememberCheckBox.isChecked()) {
+                    mPrefs.edit().putBoolean("rememberMe", false).apply();
+                }
+                mProgressBar.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mProgressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            //Intent mainMenu = new Intent(LoginActivity.this, MainMenu.class);
+                            //startActivity(MainMenu);
+                            //finish();
+                            Toast.makeText(LoginActivity.this, "SIGNED IN", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Invalid details or no connection. Please Try Again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+
+    /**
+     * If email entered in the alert dialog exists in firebase, a reset email is sent. Appropriate validation is used.
+     */
+    private void forgotPasswordDialog() {
+        mForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle("Reset Password");
+                final EditText emailInput = new EditText(LoginActivity.this);
+                emailInput.setHint("Enter email of forgotten account.");
+                emailInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(emailInput);
+                builder.setPositiveButton("Send Reset Instructions", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String email = emailInput.getText().toString().trim();
+                        if (email.isEmpty()) {
+                            Toast.makeText(LoginActivity.this, "Please type an email", Toast.LENGTH_SHORT).show();
+                        } else {
+                            mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Password reset instructions sent", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Invalid email entered. Please try again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+    }
+    /**
+     * Send the user to the sign up activity to create an account.
+     */
+    private void signUpActivity(){
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
+    }
+
+}
+
---
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/RegisterActivity.java
@@ -0,0 +1,197 @@
+package com2027.killaz.kalorie.gitfit;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    /**
+     * Register activity, allows the user to sign up.
+     */
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button mSignUpButton;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private EditText mEditTextNickname;
+    private ProgressBar mProgressBar;
+    private ArrayList<String> takenNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mSignUpButton = (Button) findViewById(R.id.createAccBtn);
+        mEditTextEmail = (EditText) findViewById(R.id.emailField);
+        mEditTextPassword = (EditText) findViewById(R.id.passwordField);
+        mEditTextNickname = (EditText) findViewById(R.id.nicknameField);
+        mProgressBar = (ProgressBar) findViewById(R.id.loginProgressBar);
+        takenNames = new ArrayList<>();
+        fetchNames();
+        createAccount();
+    }
+
+
+    /**
+     * Validates inputted fields and then ask for a nickname that is also validated and checks for duplication.
+     * Upon successful sign up, user is saved in the Authenticated Users list and the Firestore database and can login.
+     */
+    private void createAccount() {
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = mEditTextEmail.getText().toString().trim();
+                final String password = mEditTextPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    mEditTextEmail.setError("Email is required");
+                    mEditTextEmail.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mEditTextEmail.setError("Please enter a valid email");
+                    mEditTextEmail.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    mEditTextPassword.setError("Password is required");
+                    mEditTextPassword.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mEditTextPassword.setError("Minimum length of password should be 6");
+                    mEditTextPassword.requestFocus();
+                    return;
+                }
+
+                if (!mEditTextNickname.getText().toString().trim().matches("^[a-zA-Z0-9_-]{3,20}$")) {
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(RegisterActivity.this, "Invalid nickname. Nicknames can contain letters, numbers and special symbols (- and _) and must be between 3 and 20 characters", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                if (takenNames.contains(mEditTextNickname.getText().toString().trim())){
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(RegisterActivity.this, "Nickname already exists. Try a different one.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mProgressBar.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            mAuth.getCurrentUser().updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(mEditTextNickname.getText().toString().trim()).build());
+                            createFirestoreUser(mEditTextNickname.getText().toString().trim());
+                            mProgressBar.setVisibility(View.GONE);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                mProgressBar.setVisibility(View.GONE);
+                                Toast.makeText(RegisterActivity.this, "You're already registered", Toast.LENGTH_SHORT).show();
+                            } else {
+                                mProgressBar.setVisibility(View.GONE);
+                                Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+
+
+
+            }
+        });
+    }
+
+    /**
+     * Method to create the user in the collection of Users found on the Firestore database.
+     * @param username The nickname of the user.
+     */
+    private void createFirestoreUser(String username){
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("calories", 0);
+        docData.put("challenges_completed", 0);
+        docData.put("points", 0);
+        //Blank friends list.
+        docData.put("friends", Arrays.asList(true));
+        docData.put("total_distance_covered", 0);
+
+        //Creates the blank map of friend challenge.
+        Map<String, Object> friend_challenge_map = new HashMap<>();
+        friend_challenge_map.put("challenge_ref", null);
+        friend_challenge_map.put("remaining", null);
+        friend_challenge_map.put("user_ref", null);
+        docData.put("current_challenge_friend", friend_challenge_map);
+
+        //Creates a personal challenge. Taking the easiest for the start.
+        DocumentReference firstChallenge = db.document("Challenges/1");
+        Map<String, Object> self_challenge_map = new HashMap<>();
+        self_challenge_map.put("challenge_ref", firstChallenge);
+        self_challenge_map.put("remaining", 20);
+        docData.put("current_challenge_self", self_challenge_map);
+
+        db.collection("Users").document(username)
+                .set(docData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(RegisterActivity.this, "Account created successfully. You can now login.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, "Account creation failed, please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    /**
+     * Fetches all names from the database in order to make sure the nickname the user chooses is not a duplicate.
+     */
+    private void fetchNames(){
+        db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        takenNames.add(document.getId());
+                    }
+                }
+            }
+        });
+    }
+}
