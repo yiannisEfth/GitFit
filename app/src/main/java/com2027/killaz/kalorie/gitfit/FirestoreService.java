ffa7d609512d16b28f58bebd2dfcdbd76891a8eb
DavidD16 <47540905+DavidD16@users.noreply.github.com>
2019-02-19 22:24:54 +0000

    started implementing step sensor

--- app/src/main/java/com2027/killaz/kalorie/gitfit/FirestoreService.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/FirestoreService.java
@@ -1,16 +1,23 @@
 package com2027.killaz.kalorie.gitfit;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class FirestoreService extends Service {
+public class FirestoreService extends Service implements SensorEventListener {
 
     private FirebaseUser currentUser;
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
 
     public FirestoreService() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -20,8 +27,25 @@ public class FirestoreService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        }
+
+        sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         Log.i("SERVICE STARTED-USER:", currentUser.getDisplayName());
         return START_NOT_STICKY;
+    }
+
+
+    /**
+     * Unregister the sensors when service ends
+     */
+    @Override
+    public void onDestroy() {
+        sensorManager.unregisterListener(this);
     }
 
     @Override
@@ -30,4 +54,13 @@ public class FirestoreService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.i("STEP_SENSOR:", String.valueOf(event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
