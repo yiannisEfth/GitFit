5eff407baecf36ee88b0e06c68d52acd3b501f0c
Chris Dueck <cd00577@surrey.ac.uk>
2019-02-20 22:04:57 +0000

    Added further ui features for home
    Fixed previous version and extracted strings. 3 buttons added, no functionality yet.

--- app/src/main/java/com2027/killaz/kalorie/gitfit/HomeFragment.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/HomeFragment.java
@@ -1,5 +1,6 @@
 package com2027.killaz.kalorie.gitfit;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
 
@@ -15,4 +17,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Button btn1 = (Button) getView().findViewById(R.id.homeBtn1);
+        Button btn2 = (Button) getView().findViewById(R.id.homeBtn2);
+        Button btn3 = (Button) getView().findViewById(R.id.homeBtn3);
+    }
+
 }
