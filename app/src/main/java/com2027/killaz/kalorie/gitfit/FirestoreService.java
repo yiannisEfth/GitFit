abedd9ad3d5dca0108e9bb4b44eee7df26d24672
yiannisefthyvoulou <yiannisefth@outlook.com>
2019-02-20 19:49:54 +0000

    Finished step sensor
    The step-sensor now properly fetches the user's steps from the database and while the app is running tracks their steps and updates the database appropriately in real-time. Also included comments for all functionalities of the service.

--- app/src/main/java/com2027/killaz/kalorie/gitfit/FirestoreService.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/FirestoreService.java
@@ -1,40 +1,62 @@
 package com2027.killaz.kalorie.gitfit;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FirestoreService extends Service implements SensorEventListener {
-
+    /**
+     * Declare database and user references along with the necessary sensors and counters.
+     */
+    private Map<String, Object> data;
     private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference userRef;
     private SensorManager sensorManager;
     private Sensor stepSensor;
+    private int stepCounter;
 
+    /**
+     * Fetch the current user and their tracked variables.
+     */
     public FirestoreService() {
+        data = new HashMap<>();
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        userRef = db.collection("Users").document(currentUser.getDisplayName());
+        fetchUserSteps();
     }
 
+    /**
+     * Initialise the necessary sensors.
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
-            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         }
 
         sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
         Log.i("SERVICE STARTED-USER:", currentUser.getDisplayName());
         return START_NOT_STICKY;
     }
@@ -54,13 +76,33 @@ public class FirestoreService extends Service implements SensorEventListener {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    /**
+     * Track user steps and update the database appropriately in real-time.
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.i("STEP_SENSOR:", String.valueOf(event.values[0]));
+        stepCounter++;
+        data.put("total_distance_covered", stepCounter);
+        db.collection("Users").document(currentUser.getDisplayName()).set(data, SetOptions.merge());
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    /**
+     * Method to fetch the user steps when the app is started.
+     */
+    private void fetchUserSteps() {
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    stepCounter = documentSnapshot.getLong("total_distance_covered").intValue();
+                }
+            }
+        });
     }
 }
