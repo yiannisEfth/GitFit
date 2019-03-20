7de8937c28ae2e11f2c044e188f1c400a9317ead
valeriydutt <39168826+valeriydutt@users.noreply.github.com>
2019-03-20 23:10:08 +0000

    lmao pls work

--- app/src/main/java/com2027/killaz/kalorie/gitfit/LeaderboardsFragment.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/LeaderboardsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,5 +15,7 @@ public class LeaderboardsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.leaderboards_fragment, container, false);
+
+
     }
 }
