3b0d78b72375911304d8d6de438f429f01d101ea
yiannisefthyvoulou <yiannisefth@outlook.com>
2019-03-29 03:38:24 +0000

    Added maps and timer

--- app/src/main/java/com2027/killaz/kalorie/gitfit/LoginActivity.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/LoginActivity.java
@@ -1,10 +1,14 @@
 package com2027.killaz.kalorie.gitfit;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -41,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        checkLocationPermission();
         mAuth = FirebaseAuth.getInstance();
         mLoginButton = (Button) findViewById(R.id.loginButton);
         mSignUpButton = (Button) findViewById(R.id.signUpButton);
@@ -187,5 +192,15 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    public void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    99);
+        }
+
+    }
 }
 
--- app/src/main/java/com2027/killaz/kalorie/gitfit/TrackerFragment.java
+++ app/src/main/java/com2027/killaz/kalorie/gitfit/TrackerFragment.java
@@ -1,18 +1,83 @@
 package com2027.killaz.kalorie.gitfit;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
-public class TrackerFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.type.LatLng;
+
+public class TrackerFragment extends Fragment implements OnMapReadyCallback {
+
+    private Button startStop, reset;
+    private Chronometer timer;
+    private long pauseOffset;
+    private boolean runningTimer;
+    public GoogleMap mGoogleMap;
+    SupportMapFragment mapFragment;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tracker_fragment, container, false);
+    }
+
+    public TrackerFragment() {
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        startStop = getView().findViewById(R.id.tracker_start_stop);
+        reset = getView().findViewById(R.id.tracker_reset);
+        timer = getView().findViewById(R.id.tracker_timer);
+        timer.setBase(SystemClock.elapsedRealtime());
+
+        mapFragment = (SupportMapFragment)getFragmentManager().findFragmentById(R.id.mapView);
+        startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!runningTimer) {
+                    timer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                    timer.start();
+                    runningTimer = true;
+                }
+                else {
+                    timer.stop();
+                    pauseOffset = SystemClock.elapsedRealtime() - timer.getBase();
+                    runningTimer = false;
+                }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.setBase(SystemClock.elapsedRealtime());
+                pauseOffset = 0;
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        mapFragment.getMapAsync(this);
     }
 }
